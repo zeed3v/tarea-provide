5a4d5e77736caa7188748be9799f1d22bf3e4709
zeed3v <felipebrionese@gmail.com>
2022-04-20 21:25:23 -0400

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-3">
+      <tarea-app />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TareaApp from './components/TareaApp.vue'
+
 
 export default {
+  components: { TareaApp },
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+    TareaApp
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
